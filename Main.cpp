f40da0a8fcddf075f1a133cf8cf2e20f20b0202d
Luis Fernando Nuñez Vela <luisfernando.nvcb@gmail.com>
2025-05-16 00:35:02 -0400

    Se agrega funcion para renderizar ventana

--- Main.cpp
+++ Main.cpp
@@ -4,5 +4,32 @@
 
 int main()
 {
+
+	glfwInit();
+
+	GLFWwindow* window = glfwCreateWindow(800, 600, "Learn OpenGL", NULL, NULL);
+	if (window == NULL)
+	{
+		std::cout << "Failed to create GLFW window" << std::endl;
+		glfwTerminate();
+		return -1;
+	}
+	glfwMakeContextCurrent(window);
+
+	gladLoadGL();
+
+	glViewport(0, 0, 800, 800);
+
+	glClearColor(0.07f, 0.13f, 0.17f, 1.0f);
+	glClear(GL_COLOR_BUFFER_BIT);
+	glfwSwapBuffers(window);
+
+
+	while (!glfwWindowShouldClose(window))
+	{
+		glfwPollEvents();
+	}
+	
+	glfwTerminate();
 	return 0;
 }
