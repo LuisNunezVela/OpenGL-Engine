bdf1ffa3c8aa92deb728ff457e909a97c824e4e9
Luis Fernando Nuñez Vela <luisfernando.nvcb@gmail.com>
2025-07-18 23:10:57 -0400

    trace line with clicks

--- Main.cpp
+++ Main.cpp
@@ -1,13 +1,55 @@
 #include <iostream>
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
+#include <glm/glm.hpp>
+
+bool wireframe = false;
+bool keyPressed = false;
+
+GLfloat lineVertices[6]; // two 3D points (x, y, z)
+bool firstClick = true;
+bool drawLine = false;
+int clickCount = 0;
+
+// Converts screen coordinates to OpenGL NDC
+glm::vec2 screenToOpenGLCoords(double x, double y, int width, int height) {
+	float ndcX = (2.0f * x) / width - 1.0f;
+	float ndcY = 1.0f - (2.0f * y) / height;
+	return glm::vec2(ndcX, ndcY);
+}
+
+void mouse_button_callback(GLFWwindow* window, int button, int action, int mods) {
+	if (button == GLFW_MOUSE_BUTTON_LEFT && action == GLFW_PRESS) {
+		double xpos, ypos;
+		int width, height;
+		glfwGetCursorPos(window, &xpos, &ypos);
+		glfwGetWindowSize(window, &width, &height);
+
+		glm::vec2 pos = screenToOpenGLCoords(xpos, ypos, width, height);
+
+		if (clickCount == 0) {
+			lineVertices[0] = pos.x;
+			lineVertices[1] = pos.y;
+			lineVertices[2] = 0.0f;
+			clickCount++;
+		}
+		else if (clickCount == 1) {
+			lineVertices[3] = pos.x;
+			lineVertices[4] = pos.y;
+			lineVertices[5] = 0.0f;
+			clickCount = 0;
+			drawLine = true;
+		}
+	}
+}
 
 const char* vertexShaderSource = "#version 330 core\n"
 "layout (location = 0) in vec3 aPos;\n"
 "void main()\n"
 "{\n"
-"	gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+"	gl_Position = vec4(aPos, 1.0);\n"
 "}\0";
+
 const char* fragmentShaderSource = "#version 330 core\n"
 "out vec4 FragColor;\n"
 "void main()\n"
@@ -15,23 +57,22 @@ const char* fragmentShaderSource = "#version 330 core\n"
 "	FragColor = vec4(0.8f, 0.3f, 0.02f, 1.0);\n"
 "}\n\0";
 
-int main()
-{
+int main() {
 	glfwInit();
 
 	GLFWwindow* window = glfwCreateWindow(800, 800, "Learn OpenGL", NULL, NULL);
-	if (window == NULL)
-	{
+	if (window == NULL) {
 		std::cout << "Failed to create GLFW window" << std::endl;
 		glfwTerminate();
 		return -1;
 	}
 	glfwMakeContextCurrent(window);
+	glfwSetMouseButtonCallback(window, mouse_button_callback);
 
 	gladLoadGL();
-
 	glViewport(0, 0, 800, 800);
 
+	// Compile shaders
 	GLuint vertexShader = glCreateShader(GL_VERTEX_SHADER);
 	glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
 	glCompileShader(vertexShader);
@@ -41,54 +82,82 @@ int main()
 	glCompileShader(fragmentShader);
 
 	GLuint shaderProgram = glCreateProgram();
-
 	glAttachShader(shaderProgram, vertexShader);
 	glAttachShader(shaderProgram, fragmentShader);
-
 	glLinkProgram(shaderProgram);
-
 	glDeleteShader(vertexShader);
 	glDeleteShader(fragmentShader);
 
-	GLfloat vertices[] =
-	{
-		-0.5f, -0.5f * float(sqrt(3)) / 3, 0.0f, //esquina abajo izquierda
-		0.5f, -0.5f * float(sqrt(3)) / 3, 0.0f, //esquina abajo derecha
-		0.0f, 0.5f * float(sqrt(3)) * 2 / 3, 0.0f //esquina de arriba
+	// Square background
+	GLfloat squareVertices[] = {
+		-0.5f, -0.5f, 0.0f,
+		 0.5f, -0.5f, 0.0f,
+		 0.5f,  0.5f, 0.0f,
+
+		-0.5f, -0.5f, 0.0f,
+		 0.5f,  0.5f, 0.0f,
+		-0.5f,  0.5f, 0.0f
 	};
 
-	GLuint VAO, VBO;
-
-	glGenVertexArrays(1, &VAO);
-	glGenBuffers(1, &VBO);
-
-	glBindVertexArray(VAO);
-
-	glBindBuffer(GL_ARRAY_BUFFER, VBO);
-
-	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-
+	// VAO & VBO for square
+	GLuint squareVAO, squareVBO;
+	glGenVertexArrays(1, &squareVAO);
+	glGenBuffers(1, &squareVBO);
+	glBindVertexArray(squareVAO);
+	glBindBuffer(GL_ARRAY_BUFFER, squareVBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(squareVertices), squareVertices, GL_STATIC_DRAW);
 	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
 	glEnableVertexAttribArray(0);
-
 	glBindBuffer(GL_ARRAY_BUFFER, 0);
 	glBindVertexArray(0);
 
-	//loop principal de animacion
-	while (!glfwWindowShouldClose(window))
-	{
+	// VAO & VBO for dynamic line
+	GLuint lineVAO, lineVBO;
+	glGenVertexArrays(1, &lineVAO);
+	glGenBuffers(1, &lineVBO);
+	glBindVertexArray(lineVAO);
+	glBindBuffer(GL_ARRAY_BUFFER, lineVBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(lineVertices), NULL, GL_DYNAMIC_DRAW);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+	glEnableVertexAttribArray(0);
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
+	glBindVertexArray(0);
+
+	while (!glfwWindowShouldClose(window)) {
+		if (glfwGetKey(window, GLFW_KEY_P) == GLFW_PRESS && !keyPressed) {
+			wireframe = !wireframe;
+			keyPressed = true;
+			glPolygonMode(GL_FRONT_AND_BACK, wireframe ? GL_LINE : GL_FILL);
+		}
+		if (glfwGetKey(window, GLFW_KEY_P) == GLFW_RELEASE) {
+			keyPressed = false;
+		}
+
 		glClearColor(0.07f, 0.13f, 0.17f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT);
 		glUseProgram(shaderProgram);
-		glBindVertexArray(VAO);
-		glDrawArrays(GL_TRIANGLES, 0, 3);
+
+		// Draw background square
+		glBindVertexArray(squareVAO);
+		glDrawArrays(GL_TRIANGLES, 0, 6);
+
+		// Draw line if ready
+		if (drawLine) {
+			glBindBuffer(GL_ARRAY_BUFFER, lineVBO);
+			glBufferSubData(GL_ARRAY_BUFFER, 0, sizeof(lineVertices), lineVertices);
+			glBindVertexArray(lineVAO);
+			glDrawArrays(GL_LINES, 0, 2);
+		}
+
 		glfwSwapBuffers(window);
 		glfwPollEvents();
 	}
 
-	//cerrar todo
-	glDeleteVertexArrays(1, &VAO);
-	glDeleteBuffers(1, &VBO);
+	// Cleanup
+	glDeleteVertexArrays(1, &squareVAO);
+	glDeleteBuffers(1, &squareVBO);
+	glDeleteVertexArrays(1, &lineVAO);
+	glDeleteBuffers(1, &lineVBO);
 	glDeleteProgram(shaderProgram);
 	glfwDestroyWindow(window);
 	glfwTerminate();
